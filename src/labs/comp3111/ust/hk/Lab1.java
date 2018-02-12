e75c38ab1199d3bd952f9dd753f62b17baf49aea
ttchengab <ttchengab@connect.ust.hk>
2018-02-12 12:33:57 +0800

    Added a congratulations message

---
+++ src/labs/comp3111/ust/hk/Lab1.java
@@ -0,0 +1,11 @@
+package labs.comp3111.ust.hk;
+
+public class Lab1 {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Lab1: Introduction to Git and GitHub");
+		System.out.println("Congratulations! You learned the Git Basics!");
+	}
+
+}
